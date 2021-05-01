ce0180c027422bb1fb470d22d048dd3172580954
8bitgeek <mike@8bitgeek.net>
2021-05-01 03:10:06 -0400

    skeleton microamp module

---
+++ micropython_modules/microamp/micropython.mk
@@ -0,0 +1,9 @@
+MICROAMP_MOD_DIR := $(USERMOD_DIR)
+
+# Add all C files to SRC_USERMOD.
+SRC_USERMOD += $(MICROAMP_MOD_DIR)/microamp.c
+
+# We can add our module folder to include paths if needed
+# This is not actually needed in this example.
+CFLAGS_USERMOD += -I$(MICROAMP_MOD_DIR)
+CMICROAMP_MOD_DIR := $(USERMOD_DIR)
